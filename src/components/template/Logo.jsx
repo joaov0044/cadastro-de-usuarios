05b9b7c21ca9915aa72a2f01aab037c09597afe5
João victor <joaov0044@gmail.com>
2020-08-20 11:00:47 -0300

    dia 6

--- src/components/template/Logo.jsx
+++ src/components/template/Logo.jsx
@@ -1,10 +1,11 @@
 import './Logo.css'
-import logo from '../../assets/imgs/logo.png'
+import logo from '../../assets/imgs/john-5-removebg-preview.png'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
     <aside className="logo">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
             <img src={logo} alt="logo"/>
-        </a>
+        </Link>
     </aside>
